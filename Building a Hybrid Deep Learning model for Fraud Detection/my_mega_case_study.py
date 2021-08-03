1a229969e165614331954df99dfb7a0a180b6802
mohamedYehiamahmoud <72946957+mohamedYehiamahmoud@users.noreply.github.com>
2021-08-03 03:26:38 +0200

    Add files via upload

---
+++ Building a Hybrid Deep Learning model for Fraud Detection/my_mega_case_study.py
@@ -0,0 +1,80 @@
+#Building the unsupervised learning part 
+#importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing the dataset
+dataset = pd.read_csv('Credit_Card_Applications.csv')
+X = dataset.iloc[:, :-1].values 
+y = dataset.iloc[:, -1].values
+
+#Feature scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0,1))
+X = sc.fit_transform(X)
+
+#Training the som
+from minisom import MiniSom
+som = MiniSom(x=10, y=10, input_len= 15, sigma= 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
+#Visualising the results 
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markers[y[i]],
+         markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None',
+         markersize = 10,
+         markeredgewidth = 2)
+show()
+#finding the frauds
+mappings = som.win_map(X)
+frauds=np.concatenate((mappings[(3,6)],mappings[3,4]),axis=0)
+frauds=sc.inverse_transform(frauds)
+# supervised learning part
+#Creating the matrix of features
+customers= dataset.iloc[:, 1:].values 
+is_fraud=np.zeros(len(dataset))
+for i in range(len(dataset)):
+    if dataset.iloc[i,0] in frauds:
+        is_fraud[i]=1
+        
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+customers = sc.fit_transform(customers)
+# making ANN,import packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer with Dropout
+classifier.add(Dense(units = 2, kernel_initializer = 'uniform', activation = 'relu', input_dim = 15))
+
+
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting classifier to the Training set
+classifier.fit(customers,is_fraud,batch_size=1,epochs=2)
+#Create your classifier here
+
+# Predicting the probabilities of frauds 
+y_pred = classifier.predict(customers) 
+y_pred=np.concatenate((dataset.iloc[:,0:1].values,y_pred),axis=1)       
+y_pred = y_pred[y_pred[:,1].argsort()]
